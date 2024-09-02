77eb2dc8af66112bc030cf2c133e6ab2f7d35df0
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-02 17:58:32 +0600

    Alhamdulillah updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,12 +5,7 @@
 //         <div className='bg-[#fff] px-[26px] py-[13px] text-[#2B2B2B] text-[16px] font-semibold leading-6 capitalize rounded-[4px] m-96 max-w-[170px]'>
 //            <h2>Explore more</h2> 
 //         </div>
-//     );
-// };
 
-// export default Button;
-
-// import React from 'react';
 
 import React from 'react';
 

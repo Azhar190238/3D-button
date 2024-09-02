b6514407824aea675cbb5de1a6277327cf2709e2
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-02 17:58:26 +0600

    Alhamdulillah updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,11 +12,6 @@
 
 // import React from 'react';
 
-// const Button = () => {
-//   return (
-//     <div className='bg-[#fff] px-[26px] py-[13px] text-[#2B2B2B] text-[16px] font-semibold leading-6 capitalize rounded-[4px] m-96 max-w-[170px] border-l-4 border-b-4 border-[#2B2B2B] hover:border-l-0 hover:border-b-0 hover:border-t-2 hover:border-r-2 hover:translate-x-[-4px] hover:translate-y-[-4px] transition-all duration-300'>
-
-
 import React from 'react';
 
 const Button = () => {

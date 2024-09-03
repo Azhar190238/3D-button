5975da2e5240608981f9055b162da2bb1d56e7d1
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-03 12:53:58 +0600

    Alhamdulilah done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,17 @@
-
-
 import React from 'react';
+import { FiArrowUpRight } from 'react-icons/fi';
 
 const Button = () => {
     return (
-        <div className='my-96 mx-20 w-96 h-96'>
-           <div className='absolute '>
+        <div className='my-96 mx-20 w-96 h-96 bg-[#D9D9D9]'>
+            <div className='relative border-2 h-12 border-red-500 max-w-[179px]  hover:text-white group'>
+                <div className='flex font-chakra absolute bg-[#fff] hover:bg-red-500 text-[16px] px-6 whitespace-pre py-[11px] font-semibold bottom-2 left-2 transition-all duration-300 group-hover:bottom-0 group-hover:left-0'>
+                    <p className=''>Explore More</p>
+                    <FiArrowUpRight className="text-red-500 mt-1 ml-2 text-xl font-extrabold transition-transform duration-300 group-hover:rotate-45 group-hover:text-white" />
+                </div>
             </div> 
         </div>
     );
 };
 
 export default Button;
-
